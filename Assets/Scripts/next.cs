af4fe6f2a162dbbd1d708c9aec2e881a7c16fcf5
asalkey82 <89543219+asalkey82@users.noreply.github.com>
2024-03-15 05:16:10 -0400

    sound added

--- Assets/Scripts/next.cs
+++ Assets/Scripts/next.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class changeScene : MonoBehaviour
+public class next : MonoBehaviour
 {
    
     public string sceneToLoad; // Name of the scene to load
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("hand"))
+        if (other.gameObject.CompareTag("wall"))
         {
             Debug.Log("Hand interaction detected, changing scene...");
             SceneManager.LoadScene(sceneToLoad); // Load the specified scene
